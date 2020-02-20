82e21213dddf97cf5ef88649a2afeadde3d98a12
Naveen B Kulkarni <naveenkulkarni029@gmail.com>
2020-02-20 10:49:27 +0530

    created bean for jwttoken

--- src/main/java/org/nbk/eureka/zuul/security/SecurityTokenConfig.java
+++ src/main/java/org/nbk/eureka/zuul/security/SecurityTokenConfig.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.nbk.eureka.zuul.security.model.JwtConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -24,8 +25,12 @@ public class SecurityTokenConfig extends WebSecurityConfigurerAdapter {
 		.authenticationEntryPoint((req, rsp, e) -> rsp.sendError(HttpServletResponse.SC_UNAUTHORIZED)).and()
 		.addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
 		.authorizeRequests().antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
-		.antMatchers("/api/v1/movies-catalog-api" + "/admin").hasRole("ADMIN")
-		.anyRequest().authenticated();
+		.antMatchers("/api/v1/movies-catalog-api" + "/admin").hasRole("ADMIN").anyRequest().authenticated();
+    }
+
+    @Bean
+    public JwtConfig jwtConfig() {
+	return new JwtConfig();
     }
 
 }
